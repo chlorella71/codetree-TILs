facf33766ed2af20f8c9c77910cb1ceec33c36ae
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-03-11 21:37:55 +0900

    Title: 입력받아 계산 2, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240311/입력받아 계산 2/input-calculate-2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt(), b = in.nextInt();
+        System.out.print(a*b);
+    }
+}
