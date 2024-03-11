f017a4e5138919361165c7f9ab9a94e388f25c9c
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-03-11 21:21:29 +0900

    Title: 실수 받아 그대로 출력, Time: 147ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240311/실수 받아 그대로 출력/enter-real-value-and-print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+        double n = in.nextDouble();
+        System.out.printf("%.2f", n);
+    }
+}
