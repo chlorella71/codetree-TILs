dd9d15c4e364922a78d7dce6138957301fa7e7a4
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-03-15 20:40:51 +0900

    Title: 전화번호 바꾸기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240315/전화번호 바꾸기/change-phone-number.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+        in.useDelimiter("-");
+        int a = in.nextInt();
+        int xxxx = in.nextInt(), yyyy = in.nextInt();
+        System.out.print("0"+a+"-"+yyyy+"-"+xxxx);
+    }
+}
