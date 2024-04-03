fb42c978969ab4d053c114645adee15a0e203322
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-04-03 21:59:58 +0900

    Title: 물의 상태, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240403/물의 상태/state-of-water.java
+++ 240403/물의 상태/state-of-water.java
@@ -10,7 +10,7 @@ public class Main {
 
         if ( a < 0 ) {
             System.out.print("ice");
-        } else if ( a > 100 ) {
+        } else if ( a >= 100 ) {
             System.out.print("vapor");
         } else {
             System.out.print("water");
