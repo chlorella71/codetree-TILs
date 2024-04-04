10f4452c055936bb35b6aff73e91fb7ec16a9d0c
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-04-04 21:27:07 +0900

    Title: 시력 검사 2, Time: 126ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240404/시력 검사 2/eye-test-2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+
+        Double a = in.nextDouble();
+
+        if (a > 1.0) {
+            System.out.print("High");
+        } else if ( a > 0.5 ) {
+            System.out.print("Middle");
+        } else {
+            System.out.print("Low");
+        }
+    }
+}
