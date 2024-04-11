849ebbc0ec5b34bd1fef016ab609048d2d215e9b
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-04-11 21:17:20 +0900

    Title: 4가지 관계연산자, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240411/4가지 관계연산자/four-relational-operators.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt(), b = in.nextInt();
+
+        System.out.println(a >= b ? 1 : 0);
+        System.out.println(a > b ? 1 : 0);
+        System.out.println(a <= b ? 1 : 0);
+        System.out.println(a < b ? 1 : 0);
+    }
+}
