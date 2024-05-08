ec75356e404d810dba9bf317fc31357b5862ed78
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-05-08 10:31:12 +0900

    Title: 두 사람, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 240508/두 사람/two-person.java
+++ 240508/두 사람/two-person.java
@@ -5,11 +5,15 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner in = new Scanner(System.in);
         
-        int aa = in.nextInt();
-        String ag = in.nextLine();
+        // 한 줄에 입력을 받고 공백으로 분리하여 배열에 저장
+        String[] input1 = in.nextLine().split(" ");
+        String[] input2 = in.nextLine().split(" ");
         
-        int ba = in.nextInt();
-        String bg = in.nextLine();
+        // 각각의 요소를 정수로 변환
+        int aa = Integer.parseInt(input1[0]);
+        String ag = input1[1];
+        int ba = Integer.parseInt(input2[0]);
+        String bg = input2[1];
 
         if (( aa >= 19 && ag.equals("M")) || ( ba >= 19 && bg.equals("M"))){
             System.out.print(1);
