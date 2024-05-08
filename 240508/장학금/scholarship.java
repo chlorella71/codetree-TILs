c754fd715086483fb984d4d6710daad69b7aa714
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-05-08 09:51:52 +0900

    Title: 장학금, Time: 126ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240508/장학금/scholarship.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt(), b = in.nextInt();
+
+        if ( a >= 90) {
+            if ( b >= 95) {
+                System.out.print(100000);
+            } else if ( b >= 90) {
+                System.out.print(50000);
+            } 
+        } else {
+                System.out.print(0);
+            }
+    }
+}
