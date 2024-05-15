84517f319127fe2ca9195c958d42a3afff8bb4f1
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-05-15 22:07:44 +0900

    Title: 코로나 메뉴얼, Time: 157ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240515/코로나 메뉴얼/covid-manual.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+
+        char A = in.next().charAt(0);
+        int a = in.nextInt();
+        char B = in.next().charAt(0);
+        int b = in.nextInt();
+        char C = in.next().charAt(0);
+        int c = in.nextInt();
+        
+        if ( A == 'Y' && a >=37 ) {
+            if ( (B == 'Y' && b >= 37) || (C == 'Y' && c >= 37)) {
+                System.out.print("E");
+            }
+        } else if ((B == 'Y' && b >=37) && (C == 'Y' && c >= 37)) {
+                System.out.print("E");
+        }
+        
+    }
+}
