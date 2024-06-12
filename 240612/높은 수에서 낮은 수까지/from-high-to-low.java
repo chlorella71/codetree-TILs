066c9eefc87e0a1f8a6c755e91a14a8d87a4e4f1
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-06-12 23:27:46 +0900

    Title: 높은 수에서 낮은 수까지, Time: 131ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240612/높은 수에서 낮은 수까지/from-high-to-low.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt(), b = in.nextInt();
+
+        if ( a > b) {
+            for (int i = a; i <= b; i--) {
+                System.out.print(i + " ");
+            }
+        } else if ( b > a ) {
+            for (int i = b; i >= a; i--) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
