27bc814fa9c9da737d785d78a7258de917532b11
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-06-14 05:05:53 +0900

    Title: 100점 도달하기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240614/100점 도달하기/reach-100-point.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        for ( int i = n; i <= 100; i++) {
+            if ( i >= 90) {
+                System.out.print("A");
+            } else if ( i >= 80) {
+                System.out.print("B");
+            } else if ( i >= 70) {
+                System.out.print("C");
+            } else if ( i >= 60) {
+                System.out.print("D");
+            } else if ( i < 60) {
+                System.out.print("F");
+            }
+            System.out.print(" ");
+        }
+    }
+}
