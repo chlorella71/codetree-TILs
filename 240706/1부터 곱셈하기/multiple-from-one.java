891aedc06525dda355fc68de6997d87e5a1471e4
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-07-06 23:30:30 +0900

    Title: 1부터 곱셈하기, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240706/1부터 곱셈하기/multiple-from-one.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        int prod = 1;
+
+        for (int i = 1; i <= 10; i++) {
+            prod *= i;
+            if (prod >= n ) {
+                System.out.print(i);
+                break;
+            }
+        }
+    }
+}
