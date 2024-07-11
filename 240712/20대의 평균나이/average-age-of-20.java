d4738cb74869028d0d3f88cf1d144502dd75b891
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-07-12 00:26:09 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240712/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner in = new Scanner(System.in);
+        double sum = 0;
+        int cnt = 0;
+
+        while (true) {
+            int n = in.nextInt();
+            if ( 20<= n && n <= 29) {
+            sum += n;
+            cnt++;
+            continue;
+            } else if ( n > 20 || n >= 30) {
+                break;
+            }
+        }
+
+        System.out.printf("%.2f", sum/cnt);
+    }
+}
