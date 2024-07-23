977267ca34805ddec9a1e1eff406b4660f27f142
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-07-23 20:07:59 +0900

    Title: 직각 삼각형 출력, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240723/직각 삼각형 출력/Right-triangle-and-output.java
+++ 240723/직각 삼각형 출력/Right-triangle-and-output.java
@@ -17,7 +17,7 @@ public class Main {
             }
         } else {
             for (int i = 1; i < n; i++) {
-                for (int j = 1; j <= (2*i+1); j++) {
+                for (int j = 1; j <= 2*i+1; j++) {
                     System.out.print('*');
                 }
                 System.out.println();
