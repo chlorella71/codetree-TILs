42af547b31b5bec64e349d6a1dda7d121f0433e9
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-07-23 15:03:15 +0900

    Title: 소수 판별기, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240723/소수 판별기/prime-number-discriminator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        boolean satisfied = true;
+
+        for (int i = 2; i < n; i++) {
+            if ( n % i == 0) {
+                satisfied = false;
+            }
+        }
+        if ( satisfied == true) {
+            System.out.print('P');
+        } else {
+            System.out.print('C');
+        }
+    }
+}
