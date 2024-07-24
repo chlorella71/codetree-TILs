5297799a55e9d1565c1fc4a980b502cb16302147
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-07-24 19:15:28 +0900

    Title: 몫 출력하기, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 240724/몫 출력하기/print-share.java
+++ 240724/몫 출력하기/print-share.java
@@ -9,13 +9,13 @@ public class Main {
         int cnt = 0;
         while (true) {
             int n= in.nextInt();
-            cnt++;
             if (n % 2 !=0) {
             continue;
             } else {
                 System.out.println(n / 2);
+                cnt++;
             }
-            if ( cnt >=4) {
+            if ( cnt >=3) {
                 break;
             }
         }
