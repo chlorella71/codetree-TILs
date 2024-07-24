ebf39900a259e9b1b06fe22aa17894d2aa2d67fd
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-07-24 18:08:02 +0900

    Title: 별표 출력하기 3, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240724/별표 출력하기 3/print-star-3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j<=n-i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= 2*i-1; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
