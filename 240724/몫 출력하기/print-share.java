1c302e499dcfb27ed7a5925e29b2bccfa78e2bb1
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-07-24 19:08:40 +0900

    Title: 몫 출력하기, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240724/몫 출력하기/print-share.java
+++ 240724/몫 출력하기/print-share.java
@@ -7,13 +7,16 @@ public class Main {
         Scanner in = new Scanner(System.in);
 
         int cnt = 0;
-        while (cnt < 4) {
+        while (true) {
             int n= in.nextInt();
             cnt++;
             if (n % 2 !=0) {
                 continue;
             } else {
                 System.out.println(n / 2);
+            }
+            if ( cnt >= 3) {
+                break;
             }
         }
     }
