cad410516f498f6843e3a9ecc90d77823427a4e6
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-07-26 19:17:06 +0900

    Title: 별표 출력하기 8, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240726/별표 출력하기 8/print-star-8.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                for (int j = 1; j <= i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            } else {
+                System.out.print("* ");
+                System.out.println();
+            }
+        }
+    }
+}
