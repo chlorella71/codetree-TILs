749ef5a22e5be7e3fe2aa30cd2d8b4d719fa8291
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-07-27 17:32:47 +0900

    Title: 별표 출력하기 12, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240727/별표 출력하기 12/print-star-12.java
+++ 240727/별표 출력하기 12/print-star-12.java
@@ -9,7 +9,7 @@ public class Main {
 
         for (int i= 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                if (i ==1 || (j % 2 != 0 && j>= i)) {
+                if (i ==1 || (j % 2 == 0 && j>= i)) {
                     System.out.print("* ");
                 } else {
                     System.out.print("  ");
