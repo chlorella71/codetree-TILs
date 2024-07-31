ef39efe1b219c093f56a347628dccaee92600d94
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-08-01 00:17:34 +0900

    Title: 좌우반전시킨 정사각형, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240801/좌우반전시킨 정사각형/left-right-inverted-square.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        // for (int i = 1; i <= n; i++) {
+        //     for (int j = 1; j <= n; j++) {
+        //         System.out.print(i*j);
+        //     }
+        //     System.out.println();
+        // }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j >=1 ; j--) {
+                System.out.print(i*j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
