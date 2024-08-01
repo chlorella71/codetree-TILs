d0b98b0b7fcc3a810b5e9fea2bddfbd023c8890d
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-08-01 13:28:57 +0900

    Title: 좌우 지그재그 출력, Time: 230ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240801/좌우 지그재그 출력/print-lr-zigzag.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int cnt = 1;
+
+        for (int i= 1; i<= n; i++) {
+            for (int j =1; j<=n; j++) {
+                if (i % 2 !=0) {
+                    System.out.print(cnt+ " ");
+                    if ( j== n) {
+                        cnt+=n;
+                    } else {
+                        cnt++;
+                    }
+                } else {
+                    System.out.print(cnt+ " ");
+                    if (j ==n) {
+                        cnt += n;
+                    } else {
+                        cnt--;
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
