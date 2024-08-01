d2c70129369aff156eeee0c4d422a213d6d7b432
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-08-01 13:12:56 +0900

    Title: 모양대로 숫자 출력하기 2, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240801/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int cnt = 2;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j<= n; j++) {
+                System.out.print(cnt+ " ");
+                cnt +=2;
+                if (cnt > 8) {
+                    cnt = 2;
+                }
+            }
+            System.out.println();
+        }
+    }
+}
