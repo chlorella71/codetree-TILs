0115ea917e75fa26f361e91612fcbba104152642
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-08-03 01:51:30 +0900

    Title: 정상적인 구구단, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240803/정상적인 구구단/normal-multiple.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+
+        int n= in.nextInt();
+
+        for (int i =1; i<=n; i++) {
+            for (int j=1; j <=n; j++) {
+                int k = i* j;
+                System.out.print(i+" * "+ j +" = " +k);
+                if ( j < n) {
+                    System.out.print(", ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
