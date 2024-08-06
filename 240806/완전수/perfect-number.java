1c5029c51cb5be45d6c69df4164dc052a7365d80
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-08-06 20:18:20 +0900

    Title: 완전수, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

--- 240806/완전수/perfect-number.java
+++ 240806/완전수/perfect-number.java
@@ -10,6 +10,7 @@ public class Main {
         int sum = 0;
 
         for (int i = start; i<= end; i++) {
+            sum = 0;
             for ( int j= 1; j < i; j++) {
                 if ( i % j == 0) {
                     sum += j;
