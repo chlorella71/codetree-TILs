73d3d0b009dcac436a84556551ca7b00092d7ec2
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-08-06 20:00:15 +0900

    Title: 3n + 1 수열 m번, Time: 146ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240806/3n + 1 수열 m번/3n-plus-1-sequence-m-times.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+
+        int m = in.nextInt();
+
+        for (int i = 1; i<= m; i++) {
+            int n= in.nextInt();
+            int cnt= 0;
+
+            while (true) {
+                if ( n % 2 ==0) {
+                    n /=2;
+                } else {
+                    n *=3;
+                    n +=1;
+                }
+                cnt++;
+                if ( n == 1) {
+                    System.out.println(cnt);
+                    break;
+                }
+            }
+        }
+    }
+}
