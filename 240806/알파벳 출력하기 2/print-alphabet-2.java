c41ad8e9aea09ab11802e79edaf3bfe0e72805e4
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-08-06 19:46:26 +0900

    Title: 알파벳 출력하기 2, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240806/알파벳 출력하기 2/print-alphabet-2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        char cnt = 'A';
+
+        for (int i = 1; i<= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if ( i > j) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print(cnt+" ");
+                    cnt++;
+                    if (cnt > 'Z') {
+                        cnt = 'A';
+                    }
+                }
+            }
+            System.out.println();
+        }
+    }
+}
