6fcf8877ab9649b3c703568fc50b9a436dcee664
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-08-07 14:43:00 +0900

    Title: 10개 입력받기, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240807/10개 입력받기/receive-10-inputs.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+
+        int[] arr = new int[10];
+        double cnt = 0;
+        int sum = 0;
+
+        for (int i = 0; i<= 9; i++) {
+            arr[i] = in.nextInt();
+            if (arr[i] == 0) {
+                System.out.printf("%s %.1f", sum, sum/cnt);
+                break;
+            }
+            sum += arr[i];
+            cnt++;
+        }
+    }
+}
