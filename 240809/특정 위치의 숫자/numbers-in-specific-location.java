1ee874a611c37c70260bcfcb1162fb4c61ca78f0
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-08-09 18:22:14 +0900

    Title: 특정 위치의 숫자, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240809/특정 위치의 숫자/numbers-in-specific-location.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[10];
+
+        for (int i = 0; i< 10; i++) {
+            arr[i] = in.nextInt();
+
+        }
+        System.out.print(
+            arr[2]+arr[4]+arr[9]
+        );
+    }
+}
