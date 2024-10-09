35cc32016c08cf35c48b21eea7ab2bb9f4d3e40d
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-10-10 00:14:17 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 227ms, Memory: 13MB, Status: Passed - CodeTree

--- 241010/중복되지 않는 숫자 중 최대/max-of-unique-number.java
+++ 241010/중복되지 않는 숫자 중 최대/max-of-unique-number.java
@@ -12,7 +12,6 @@ public class Main {
         int[] arr = new int[1000];
         int[] arr2 = new int[1000];
         int idx = 0;
-        int cnt = 0;
         boolean x = false;
         boolean y = true;
 
@@ -46,43 +45,6 @@ public class Main {
                 }
             }
             System.out.print(max);
-        }
-
-        // for (int i = 0; i< n; i++) {
-        //     x = false;
-        //     if (i != n-1) {
-        //         for (int j = 0; j< n; j++) {
-        //             if (i == j) {
-        //                 continue;
-        //             }else if (arr[i] == arr[j]) {
-        //                 x = true;
-        //             }
-        //             if (j == n-1 && x == false) {
-        //                 arr2[idx] = arr[i];
-        //                 idx++;
-        //                 cnt++;
-        //             }   
-        //         }
-        //     } else {
-        //         for (int k = 0; i < n-1; i++) {
-        //             if (arr[k] == arr[n-1]) {
-        //                 y = true;
-        //             } 
-        //         }
-        //         if ( y == false) {
-        //             arr[idx] = arr[n-1];
-        //         }
-        //     }
-        // }
-        // if (cnt == 0 ) {
-        //     System.out.print(-1);
-        // } else {
-        //     for (int i = 0; i <= idx; i++) {
-        //         if (max < arr2[i]) {
-        //             max = arr2[i];
-        //         }
-        //     }
-        //     System.out.print(max);
-        // }
+        }     
     }
 }
