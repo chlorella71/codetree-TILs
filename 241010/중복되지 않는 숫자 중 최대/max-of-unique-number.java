fb962d8b82724fc032bd75e92ebf6bb625b7e475
chlorella71 <150788023+chlorella71@users.noreply.github.com>
2024-10-10 00:13:11 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 252ms, Memory: 13MB, Status: Passed - CodeTree

--- 241010/중복되지 않는 숫자 중 최대/max-of-unique-number.java
+++ 241010/중복되지 않는 숫자 중 최대/max-of-unique-number.java
@@ -8,15 +8,13 @@ public class Main {
         int n= in.nextInt();
         final int INT_MIN = Integer.MIN_VALUE;
         int max = INT_MIN;
-        // int max2 = INT_MIN;
 
         int[] arr = new int[1000];
         int[] arr2 = new int[1000];
         int idx = 0;
         int cnt = 0;
         boolean x = false;
-        boolean y = false;
-        // int y = -1;
+        boolean y = true;
 
         for (int i = 0; i< n; i++) {
             arr[i] = in.nextInt();
@@ -24,56 +22,59 @@ public class Main {
 
         for (int i = 0; i< n; i++) {
             x = false;
-            if (i != n-1) {
-                for (int j = 0; j< n; j++) {
-                    if (i == j) {
-                        continue;
-                    }else if (arr[i] == arr[j]) {
+            for (int j = 0; j <n; j++) {
+                if ( i == j) {
+                    continue;
+                } else {
+                    if (arr[i] == arr[j]) {
                         x = true;
                     }
-                    if (j == n-1 && x == false) {
-                        arr2[idx] = arr[i];
-                        idx++;
-                        cnt++;
-                    }   
-                }
-            } else {
-                for (int k = 0; i < n-1; i++) {
-                    if (arr[k] == arr[n-1]) {
-                        y = true;
-                        continue;
-                    } 
-                }
-                if ( y == false) {
-                    arr[idx] = arr[n-1];
                 }
             }
+            if (x == false) {
+                arr2[idx] = arr[i];
+                idx++;
+                y = false;
+            }
         }
-        if (cnt == 0 ) {
+        if (y == true) {
             System.out.print(-1);
         } else {
-            for (int i = 0; i <= idx; i++) {
+            for (int i = 0; i<= idx; i++) {
                 if (max < arr2[i]) {
                     max = arr2[i];
                 }
             }
             System.out.print(max);
         }
-    }
-
 
         // for (int i = 0; i< n; i++) {
-        //     for (int j =i+1; j<n; j++) {
-        //         if (arr[i] == arr[j]) {
-        //             continue;
+        //     x = false;
+        //     if (i != n-1) {
+        //         for (int j = 0; j< n; j++) {
+        //             if (i == j) {
+        //                 continue;
+        //             }else if (arr[i] == arr[j]) {
+        //                 x = true;
+        //             }
+        //             if (j == n-1 && x == false) {
+        //                 arr2[idx] = arr[i];
+        //                 idx++;
+        //                 cnt++;
+        //             }   
         //         }
-        //         if (j == n-1) {
-        //             arr2[idx] = arr[i];
-        //             idx++;
+        //     } else {
+        //         for (int k = 0; i < n-1; i++) {
+        //             if (arr[k] == arr[n-1]) {
+        //                 y = true;
+        //             } 
+        //         }
+        //         if ( y == false) {
+        //             arr[idx] = arr[n-1];
         //         }
         //     }
         // }
-        // if ( x == false) {
+        // if (cnt == 0 ) {
         //     System.out.print(-1);
         // } else {
         //     for (int i = 0; i <= idx; i++) {
@@ -83,4 +84,5 @@ public class Main {
         //     }
         //     System.out.print(max);
         // }
+    }
 }
